c13b554c7deed8e8f4268d7483cb65219cb5777e
Brian Anderson <andersrb@gmail.com>
2019-10-15 17:29:07 +0800

    .

--- is13-runtime.rs
+++ is13-runtime.rs
@@ -2,19 +2,27 @@ use bytes::Bytes;
 use std::io::Read;
 use std::env;
 use std::fs::File;
+use ckb_vm::SparseMemory;
+use b_error::{BResult, ResultExt};
 
 fn main() {
+    b_error::main(run)
+}
+
+fn run() -> BResult<()> {
     let args: Vec<Bytes> = env::args().map(|a| a.into()).collect();
 
-    let mut file = File::open("is13-client").expect("open is13-client");
+    let mut file = File::open("is13-client").e()?;
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
+    file.read_to_end(&mut buffer).e()?;
     let buffer = Bytes::from(buffer);
 
-    let r = ckb_vm::run::<u32, ckb_vm::SparseMemory<u32>>(&buffer, &args[..]).unwrap();
+    let r = ckb_vm::run::<u32, SparseMemory<u32>>(&buffer, &args[..]).e()?;
     match r {
         1 => println!("{:?} is not thirteen", args[1]),
         0 => println!("{:?} is thirteen", args[1]),
         _ => panic!(""),
     }
+
+    Ok(())
 }
